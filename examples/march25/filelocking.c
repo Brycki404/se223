407f30dee9ddcb54046f56b114e0179ceefe0402
Brycki404 <dustinbk@icloud.com>
2026-04-27 13:46:53 -0400

    added

--- examples/march25/filelocking.c
+++ examples/march25/filelocking.c
@@ -1,105 +1,96 @@
 //This example demonstrates locking a file.
-//This processs forks a child. The parent locks the file and writes. The child tries to access it but has to wait.
-//It was written by Andy Mitofsky.
+//This process forks a child. The parent locks the file and writes. THe child tries to access it but has to wait.
+//It was written by Andy Mitofsky
 #include "../se233.h"
-
-void xp_fork_child(void *arg)
-{
-    printf("Child says hi \n");
-    const char *kidbuf = "pepper";
-    sleep(2);
-
-    printf("Child is trying to get lock\n");
-
-    int fd = open("veggies.txt", O_CREAT | O_APPEND | O_RDWR);
-    if (fd == -1)
-    {
-        perror("open error");
-    }
-
-    if (lockf(fd, F_LOCK, 0) == 0) {
-        if (write(fd, kidbuf, strlen(kidbuf)) == -1)
-        {
-            perror("write error");
-        }
-        printf("Child wrote\n");
-    }
-    else
-    {
-        perror("child can't get lock");
-    }
-
-    if (lockf(fd, F_ULOCK, 0) == -1) {
-        perror("lockf error");
-    }
-
-    if (close(fd) == -1) {
-        perror("close error");
-    }
-
-    printf("Child is done\n");
-
-    #ifdef _WIN32
-        // Threads must exit explicitly
-        ExitThread(0);
-    #endif
-}
-
 int main()
 {
-    int fd;
-    pid_t mypid;
-    pid_t kidpid;
-    pid_t wpid;
-    int y;
+	int fd;
+	pid_t mypid;
+	pid_t kidpid;
+	pid_t wpid;
+	int y;
 
-    kidpid = xp_fork();
-    if (kidpid == -1)
-    {
-        perror("Fork error");
-    }
-    else if (kidpid == 0)
-    {
-        #ifndef _WIN32
-            // Linux child runs here
-            xp_fork_child(NULL);
-         #endif
-    }
-    else
-    {
-        //We're in the parent
-        printf("Parent says hi\n");
-        const char * parentbuf="carrot \n";
-        fd=open("veggies.txt", O_CREAT|O_APPEND|O_RDWR);
-        if (fd==-1)
-        {
-            perror("open error");
-        }
-        if (chmod("veggies.txt", (S_IRWXU|S_IRWXG|S_IRWXO))==-1)
-        {
-            perror("fchmod error");
-        }
-        //lock the file
-        if (lockf(fd, F_LOCK, 0)==-1)
-        {
-            perror("lockf error");
-        }
-        //write to the file
-        if (write(fd, parentbuf, strlen(parentbuf))==-1)
-        {
-            perror("write error");
-        }
-        printf("Parent wrote and is holding the lock for a bit \n");
-        sleep(3);
-        //parent release the lock
-        if (lockf(fd, F_ULOCK, 0)==-1)
-        {
-            perror("lockf error");
-        }
-         printf("Parent is done \n");
-         if (close(fd)==-1)
-         {
-             perror("close error");
-         }
-    }
+	//fork the child
+	kidpid=fork();
+	if(kidpid==-1)
+	{
+		perror("Fork error");
+	}
+	if(kidpid==0)
+	{
+	//We're in the child
+		printf("Child says hi \n");
+		const char *kidbuf="pepper";
+		sleep(2);
+		//child tries to get lock
+		printf("Child tries to get lock\n");
+		fd=open("veggies.txt", O_CREAT|O_APPEND|O_RDWR);
+		if(fd==-1)
+		{
+			perror("open error");
+		}
+		if(lockf(fd, F_LOCK,0)==0)
+		{
+			if(write(fd, kidbuf, strlen(kidbuf))==-1)
+				{perror("write error");
+				}
+			printf("Child wrote \n");
+		}
+		else
+		{
+			perror("child can't get lock \n");
+		}
+		//child releases lock 
+		if(lockf(fd, F_ULOCK, 0)==-1)
+		{
+			perror("lockf error");
+		}
+		if(close(fd)==-1)
+		{
+			perror("close error \n");
+		}
+
+	}
+	else
+	{
+		//We're in the parent
+		printf("Parent says hi \n");
+		const char * parentbuf="carrot \n";
+		fd=open("veggies.txt", O_CREAT|O_APPEND|O_RDWR);
+		if(fchmod(fd, S_IRWXU|S_IRWXG|S_IRWXO)==-1)
+		{
+			perror("fchmod error");
+		}
+		//Lock file
+		if(lockf(fd, F_LOCK,0)==-1)
+		{
+			perror("lock error");
+		}
+		//write to the file
+		if(write(fd, parentbuf, strlen(parentbuf))==-1)
+		{
+			perror("write error");
+		}
+		printf("Parent wrote and is holding the lock for a bit \n");
+		sleep(3);
+		//parent releases the lock
+		if(lockf(fd, F_ULOCK, 0)==-1)
+		{
+			perror("lockf error");
+		}
+		printf("Parent is done \n");
+		if(close(fd)==-1)
+		{
+			perror("close error");
+		}
+
+		wpid=wait(&y);
+
+	}
+
+
+
+
+
+	return 0;
 }
--- examples/march25/sharedmemory.c
+++ examples/march25/sharedmemory.c
@@ -1,40 +1,42 @@
+//This example demonstrate shared memory between two processes. This is 
+//the first one. I twas written by Andy Mitofsky. Before I started
+//I created a empty file ./myshm
 #include "../se233.h"
-
 int main()
 {
-    //I think this is what was here, but I can't be sure. I don't have the original code.
-    int mid;
-    char msg2[30];
-    char (*msgptr)[30];
-    key_t mykey;
+	int mid;
+	char msg[30];
+	char (*msgptr)[30];
+	key_t mykey;
 
-    strcpy(msg2, "pineapple");
+	strcpy(msg, "pineapple");
 
-    //get the key
-    mykey=ftok("./myshm", 18);
-    if (mykey==-1)
-    {
-        perror("ftok error");
-    }
-    //Set up the shared memory
-    mid=shmget(mykey, 30, IPC_CREAT);
-    if (mid==-1)
-    {
-        perror("shmget error");
-    }
-    //Attach to memory
-    msgptr=shmat(mid, NULL, 0);
-    if (msgptr==(void*)-1)
-    {
-        perror("shmat error");
-    }
-    //Put info in memory
-    strcpy(*msgptr, msg2);
-    //detatch
-    if(shmdt(msgptr)==-1)
-    {
-        perror("Detatch error");
-    }
+	//get the key
+	mykey=ftok("./myshm", 18);
+	if(mykey==-1)
+	{
+		perror("Ftok error");
+	}
+	//Set up the shared memory
+	mid=shmget(mykey, 30, IPC_CREAT);
+        if(mid==-1)
+	{
+		perror("shmget error");
+	}	
+	//Attach to memory
+	msgptr=shmat(mid, 0,0);
+	if(msgptr==-1)
+	{
+		perror("shmat error");
+	}
 
-    return 0;
+	//Put info in memory
+	strcpy(*msgptr, msg);
+	//Detatch to memory
+	if(shmdt(msgptr)==-1)
+	{
+		perror("Detatich error");
+	}
+
+	return 0;
 }
